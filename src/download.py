06e3b1b5d7a3698730fb0bd774ae9e5e36b894fc
Sanaz <49277985+sanazamann@users.noreply.github.com>
2026-03-09 11:11:15 +0100

    canciones

--- src/download.py
+++ src/download.py
@@ -1,19 +1,26 @@
 import json
+import os
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 
 CLIENT_ID = '44a679c4d0a34b09b0b6534fa5c2d300'
 CLIENT_SECRET = '2ee21c22a3fb4ea5bf7b0963bab7cb78'
+REDIRECT_URI = 'http://localhost:8888/callback'
 
-client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET,
+    redirect_uri=REDIRECT_URI,
+    scope='playlist-read-public'
+))
 
 def obtener_canciones_populares():
-    resultados = sp.search(q='pop 2024', type='track', limit=10)
+    # Top 50 Global
+    resultados = sp.playlist_tracks('37i9dQZEVXbMDoHDwVN2tF', limit=50)
     canciones = []
 
-    for track in resultados['tracks']['items']:
+    for item in resultados['items']:
+        track = item['track']
         cancion = {
             'id': track['id'],
             'nombre': track['name'],
@@ -22,14 +29,3 @@ def obtener_canciones_populares():
         canciones.append(cancion)
 
     return canciones
-
-
-def guardar_canciones(canciones):
-    with open('data/clean/canciones_populares.json', 'w') as archivo:
-        json.dump(canciones, archivo, indent=4)
-    print(f'Se han guardado {len(canciones)} canciones.')
-
-
-if __name__ == '__main__':
-    canciones_populares = obtener_canciones_populares()
-    guardar_canciones(canciones_populares)
