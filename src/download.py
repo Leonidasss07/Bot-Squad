4648eeac10248d351cbebf0b8c94004c7e7c8686
Sanaz <49277985+sanazamann@users.noreply.github.com>
2026-03-12 13:22:24 +0100

    canciones popularess

--- src/download.py
+++ src/download.py
@@ -1,23 +1,25 @@
+import requests
 import json
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+import os
 
-CLIENT_ID = '44a679c4d0a34b09b0b6534fa5c2d300'
-CLIENT_SECRET = '2ee21c22a3fb4ea5bf7b0963bab7cb78'
-
-client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-
-
+API_KEY = '2486bf623744f4f6f8e4b2a60720a504' 
 def obtener_canciones_populares():
-    resultados = sp.search(q='pop 2024', type='track', limit=10)
+    url = 'http://ws.audioscrobbler.com/2.0/'
+    params = {
+        'method': 'chart.gettoptracks',
+        'api_key': API_KEY,
+        'format': 'json',
+        'limit': 50
+    }
+    respuesta = requests.get(url, params=params)
+    datos = respuesta.json()
     canciones = []
 
-    for track in resultados['tracks']['items']:
+    for track in datos['tracks']['track']:
         cancion = {
-            'id': track['id'],
             'nombre': track['name'],
-            'artista': track['artists'][0]['name']
+            'artista': track['artist']['name'],
+            'reproducciones': track['playcount']
         }
         canciones.append(cancion)
 
@@ -25,11 +27,14 @@ def obtener_canciones_populares():
 
 
 def guardar_canciones(canciones):
-    with open('data/clean/canciones_populares.json', 'w') as archivo:
-        json.dump(canciones, archivo, indent=4)
-    print(f'Se han guardado {len(canciones)} canciones.')
+    os.makedirs('data/clean', exist_ok=True)
+    file_path = 'data/clean/canciones_populares.json'
+    with open(file_path, 'w') as archivo:
+        for cancion in canciones:
+            archivo.write(json.dumps(cancion, ensure_ascii=False) + '\n')
+    print(f'Se han guardado {len(canciones)} canciones en {file_path}')
 
 
 if __name__ == '__main__':
-    canciones_populares = obtener_canciones_populares()
-    guardar_canciones(canciones_populares)
+    canciones = obtener_canciones_populares()
+    guardar_canciones(canciones)
