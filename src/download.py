fa11a77919c298c64e1d2a3e5291f1af21650621
Leonidasss07 <elombunaleoncio@gmail.com>
2026-03-16 08:41:02 +0100

    creacion funcion canciones julio

--- src/download.py
+++ src/download.py
@@ -30,6 +30,31 @@ def guardar_canciones(canciones):
     print(f'Se han guardado {len(canciones)} canciones.')
 
 
+##Creamos el codigo para descargar canciones mas populares de un mes
+def obtener_canciones_julio():
+    # Spotify API permite un límite máximo de 50; usar 10 para evitar errores.
+    resultados = sp.search(q='lo mas escuchado en julio 2025', type='track', limit=10)
+    canciones_populares_julio = []
+
+    for track in resultados['tracks']['items']:
+        cancion_julio = {
+            'id': track['id'],
+            'nombre': track['name'],
+            'artista': track['artists'][0]['name']
+        }
+        canciones_populares_julio.append(cancion_julio)
+
+    return canciones_populares_julio   
+
+def guardar_canciones_julio(canciones_populares_julio):
+    with open('data/clean/canciones_populares_julio.json', 'w') as archivo:
+        json.dump(canciones_populares_julio, archivo, indent=4)
+    print(f'Se han guardado {len(canciones_populares_julio)} canciones.')
+
+
 if __name__ == '__main__':
     canciones_populares = obtener_canciones_populares()
     guardar_canciones(canciones_populares)
+
+    canciones_populares_julio = obtener_canciones_julio()
+    guardar_canciones_julio(canciones_populares_julio)
