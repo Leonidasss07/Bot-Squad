2656c00dbc1bbea83d3267d4c2f02b0b58c803f3
Juan Diego <juandisoto29@gmail.com>
2026-03-19 12:57:39 +0100

    commit

--- src/download.py
+++ src/download.py
@@ -9,7 +9,8 @@ def obtener_canciones_populares():
         'method': 'chart.gettoptracks',
         'api_key': API_KEY,
         'format': 'json',
-        'limit': 50
+        'limit': 1000,
+        'page': i+1
     }
     respuesta = requests.get(url, params=params)
     datos = respuesta.json()
@@ -19,19 +20,21 @@ def obtener_canciones_populares():
         cancion = {
             'nombre': track['name'],
             'artista': track['artist']['name'],
-            'reproducciones': track['playcount']
+            'reproducciones': track['playcount'],
+            'url': track['url']
         }
         canciones.append(cancion)
 
     return canciones
 
 def obetener_artistas_populares():
+    
     url =  'http://ws.audioscrobbler.com/2.0/'
     params = {
         'method': 'chart.gettopartists',
         'format': 'json',
         'page': 1,
-        'limit': 50,
+        'limit': 1000,
         'api_key': API_KEY
     }
     respuesta = requests.get(url, params=params)
@@ -42,10 +45,11 @@ def obetener_artistas_populares():
         musico = {
             'nombre': artista['name'],
             'reproducciones': artista['playcount'],
-            'oyentes': artista['listeners']
+            'oyentes': artista['listeners'],
+            'url': artista['url']
         }
         artistas.append(musico)
-        
+            
     return artistas
 
 def guardar_artistas(artistas):
@@ -54,7 +58,7 @@ def guardar_artistas(artistas):
     with open(file_path, 'w') as archivo:
         for musico in artistas:
             archivo.write(json.dumps(musico, ensure_ascii=False) + '\n')
-    print(f'Se han guardado {len(artistas)} artisatas en {file_path}')
+    print(f'Se han guardado {len(artistas)} artistas en {file_path}')
 
 
 
@@ -67,9 +71,43 @@ def guardar_canciones(canciones):
             archivo.write(json.dumps(cancion, ensure_ascii=False) + '\n')
     print(f'Se han guardado {len(canciones)} canciones en {file_path}')
 
+def obtener_canciones_populares_julio():
+    url = 'http://ws.audioscrobbler.com/2.0/'
+    params = {
+        'method': 'album.getinfo',
+        'artist': 'KY Noraebang',
+        'album': 'July 2021\'s popular song Vol.2',
+        'api_key': API_KEY,
+        'format': 'json'
+    }
+    respuesta = requests.get(url, params=params)
+    datos = respuesta.json()
+    canciones_populares_julio = []
+
+    for track in datos['album']['tracks']['track']:
+        cancion_julio = {
+            'nombre': track['name'],
+            'artista': track['artist']['name'],
+            'duracion': track.get('duration', 'N/A')
+        }
+        canciones_populares_julio.append(cancion_julio)
+
+    return canciones_populares_julio
+
+def guardar_canciones_julio(canciones_populares_julio):
+    os.makedirs('data/clean', exist_ok=True)
+    file_path = 'data/clean/canciones_populares_julio.json'
+    with open(file_path, 'w') as archivo:
+        for cancion_julio in canciones_populares_julio:
+            archivo.write(json.dumps(cancion_julio, ensure_ascii=False) + '\n')
+    print(f'Se han guardado {len(canciones_populares_julio)} canciones en {file_path}')
+
+
 
 if __name__ == '__main__':
     canciones = obtener_canciones_populares()
     guardar_canciones(canciones)
     artistas = obetener_artistas_populares()
     guardar_artistas(artistas)
+    canciones_populares_julio = obtener_canciones_populares_julio()
+    guardar_canciones_julio(canciones_populares_julio)
